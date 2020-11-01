520063cf18956865c50034e755c16250e596a0ab
Rob Sawyer <rob@layerframe.com>
2020-11-01 18:03:03 +0900

    Config updates

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,15 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="min-h-screen flex flex-col justify-center align-center">
       <Head>
         <title>prototype</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=""></main>
+      <main className="flex flex-1 flex-col justify-center align-center bg-black"></main>
 
-      <footer className="h-100 bg-black">Powered by passion</footer>
+      <footer className="w-full h-100 bg-black">Powered by passion</footer>
     </div>
   )
 }
