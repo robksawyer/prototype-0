0f6846c8b04bd1de3ae00fb6a20b9299d1b8acb2
Rob Sawyer <rob@layerframe.com>
2020-11-01 18:07:03 +0900

    Color updates

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,9 @@ export default function Home() {
 
       <main className="flex flex-1 flex-col justify-center align-center bg-black"></main>
 
-      <footer className="w-full h-100 bg-black">Powered by passion</footer>
+      <footer className="w-full h-100 bg-red-400 text-white px-40 flex align-center justify-center">
+        Powered by passion
+      </footer>
     </div>
   )
 }
